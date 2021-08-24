eee726ea7e2351dc0fe08adb97375f532c365ff3
weaponsforge <forgeweapons@gmail.com>
2021-08-25 03:27:02 +0800

    Use .htm extension on HTML (MD) files.

--- src/domain/blog/html_viewer/index.js
+++ src/domain/blog/html_viewer/index.js
@@ -12,15 +12,16 @@ function HtmlBlogPage (props) {
 
   useEffect(() => {
     try {
-      // Use HTML syntax in MD files
+      // Use HTML syntax in .htm files
       // because using the .html extension will require manual webpack.config customization and the react app to be ejected
-      const htmlDoc = require(`../../../pages/html/${props.match.params.id}.md`)
+      const htmlDoc = require(`../../../pages/html/${props.match.params.id}.htm`)
 
       fetch(htmlDoc.default).then((response) => response.text()).then((text) => {
         const sanitizedDoc = DOMPurify.sanitize(text)
         setDoc(sanitizedDoc)
       })
     } catch (err) {
+      console.log(err.message)
       setDoc('Not found')
     }
   }, [props.history, props.match.params.id])
